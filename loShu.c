3575d3780a8f8a701f3e65e0c7ac36f9b78bf4bd
shen-c <congshen@cpp.edu>
2022-10-23 22:57:19 -0700

    initial commit outline loshu

---
+++ loShu.c
@@ -0,0 +1,18 @@
+//some function to generate random array
+
+//some function to validate array as Lo Shu
+
+int main(void){
+
+    //generate a 2d array
+
+    //check if that square is valid (part 1)
+
+    //shuffle the 2d array (part 2)
+
+    //check if shuffled square is valid (part 2)
+
+    //print iterations
+
+    //print successful square
+}
